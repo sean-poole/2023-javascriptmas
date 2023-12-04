15c2de1afe3de8906248cac6ddb077435486e53f
Sean Poole <spoole5289@gmail.com>
2023-12-04 12:19:49 -0800

    Day 4 submission.

---
+++ 12.04/main.js
@@ -0,0 +1,42 @@
+/** uncomment one of these **/
+import OpenAI from "openai"
+// import { HfInference, textGeneration } from '@huggingface/inference'
+
+/**
+ * 🎄 Challenge:
+ * 1. When clicked, the doors should open
+ *    to reveal a festive joke.
+ * 
+ * 🎁 hint.md for help!
+**/
+
+document.getElementById('window-container').addEventListener('click', function () {
+  async function getJoke() {
+    const openai = new OpenAI({ 
+      apiKey: process.env.OPENAI_API_KEY,
+      dangerouslyAllowBrowser: true 
+    });
+
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "user",
+          content: "Tell me a Christmas-themed dad joke."
+        }
+      ],
+      model: "gpt-3.5-turbo-1106"
+    });
+
+    const leftDoor = document.querySelector('.left-door');
+    const rightDoor = document.querySelector('.right-door');
+    const jokeDisplay = document.querySelector('.joke-display');
+    const jokeText = document.getElementById("joke-display");
+  
+    leftDoor.style.animation = "left-open 0.3s forwards";
+    rightDoor.style.animation = "right-open 0.3s forwards";
+    jokeDisplay.style.animation = "display-joke 0.3s forwards";
+    jokeText.innerHTML = completion.choices[0].message.content;
+  }
+
+  getJoke();
+});
